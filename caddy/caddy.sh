f0f185b87a58693e73f04cde12f9f9d7e1a87fbd
lost1984 <yteinstein@gmail.com>
2021-04-29 15:52:53 +0800

    Update caddy.sh

--- caddy/caddy.sh
+++ caddy/caddy.sh
@@ -2,11 +2,11 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin; export PATH
 # 设置变量
 bincaddy=${bincaddy:-https://raw.githubusercontent.com/lost1984/debian/main/caddy/bin/caddy}
-configcaddy=${configcaddy:-https://raw.githubusercontent.com/lost1984/debian/main/caddy/etc/Caddyfile}
+configcaddy=${configcaddy:-https://raw.githubusercontent.com/lost1984/debian/main/caddy/etc/caddy.json}
 sercaddy=${sercaddy:-https://raw.githubusercontent.com/lost1984/debian/main/caddy/etc/caddy.service}
 #安装caddy主程序
 wget -O /usr/local/bin/caddy $bincaddy
-wget -O /usr/local/etc/caddy/Caddyfile $configcaddy
+wget -O /usr/local/etc/caddy/caddy.json $configcaddy
  wget -O /etc/systemd/system/caddy.service $sercaddy
  chmod 777 /usr/local/bin/caddy
 #添加自启动
