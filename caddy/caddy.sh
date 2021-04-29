c64a1bdd57e49b3a4ef00b145d4a7992538822e2
lost1984 <yteinstein@gmail.com>
2021-04-29 15:31:00 +0800

    Update caddy.sh

--- caddy/caddy.sh
+++ caddy/caddy.sh
@@ -5,7 +5,7 @@ bincaddy=${bincaddy:-https://raw.githubusercontent.com/lost1984/debian/master/ca
 configcaddy=${configcaddy:-https://raw.githubusercontent.com/lost1984/debian/master/caddy/etc/Caddyfile}
 sercaddy=${sercaddy:-https://raw.githubusercontent.com/lost1984/debian/master/caddy/etc/caddy.service}
 #安装caddy主程序
-caddy_install(){
+#caddy_install(){
 # caddyconfig
     wget -O /usr/local/bin/caddy $bincaddy
     wget -O /usr/local/etc/caddy/Caddyfile $configcaddy
@@ -13,4 +13,4 @@ caddy_install(){
     chmod 777 /usr/local/bin/caddy
     #添加自启动
     systemctl enable caddy && systemctl restart caddy
-}
+#}
