5473c3d1a93e67cc99bebc09cfeccaad6ba746e8
lost1984 <yteinstein@gmail.com>
2021-04-29 15:28:37 +0800

    Update caddy.sh

--- caddy/caddy.sh
+++ caddy/caddy.sh
@@ -1,15 +1,12 @@
-
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin; export PATH
 # 设置变量
-
 bincaddy=${bincaddy:-https://raw.githubusercontent.com/lost1984/debian/master/caddy/bin/caddy}
 configcaddy=${configcaddy:-https://raw.githubusercontent.com/lost1984/debian/master/caddy/etc/Caddyfile}
 sercaddy=${sercaddy:-https://raw.githubusercontent.com/lost1984/debian/master/caddy/etc/caddy.service}
-
 #安装caddy主程序
 caddy_install(){
-    # caddyconfig
+# caddyconfig
     wget -O /usr/local/bin/caddy $bincaddy
     wget -O /usr/local/etc/caddy/Caddyfile $configcaddy
     wget -O /etc/systemd/system/caddy.service $sercaddy
