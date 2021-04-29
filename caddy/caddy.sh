392281d81be1afd0b6dc5a8f3e5c27750cca2b3b
lost1984 <yteinstein@gmail.com>
2021-04-29 15:46:27 +0800

    Update caddy.sh

--- caddy/caddy.sh
+++ caddy/caddy.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin; export PATH
 # 设置变量
-bincaddy=${bincaddy:-https://raw.githubusercontent.com/lost1984/debian/master/caddy/bin/caddy}
-configcaddy=${configcaddy:-https://raw.githubusercontent.com/lost1984/debian/master/caddy/etc/Caddyfile}
-sercaddy=${sercaddy:-https://raw.githubusercontent.com/lost1984/debian/master/caddy/etc/caddy.service}
+bincaddy=${bincaddy:-https://raw.githubusercontent.com/lost1984/debian/main/caddy/bin/caddy}
+configcaddy=${configcaddy:-https://raw.githubusercontent.com/lost1984/debian/main/caddy/etc/Caddyfile}
+sercaddy=${sercaddy:-https://raw.githubusercontent.com/lost1984/debian/main/caddy/etc/caddy.service}
 #安装caddy主程序
 wget -O /usr/local/bin/caddy $bincaddy
 wget -O /usr/local/etc/caddy/Caddyfile $configcaddy
-wget -O /etc/systemd/system/caddy.service $sercaddy
-chmod 777 /usr/local/bin/caddy
+ wget -O /etc/systemd/system/caddy.service $sercaddy
+ chmod 777 /usr/local/bin/caddy
 #添加自启动
 systemctl enable caddy && systemctl restart caddy
 
